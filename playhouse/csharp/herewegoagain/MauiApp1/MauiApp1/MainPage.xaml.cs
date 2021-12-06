34f01d2c7e18db8ca40ce10ba562f109a23c88bb
Emilia Holmström <e-holmstrom@hotmail.com>
2021-12-06 15:41:28 +0100

    Vette attan

---
+++ playhouse/csharp/herewegoagain/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using Microsoft.Maui;
+//using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
+using Application = Microsoft.Maui.Controls.Application;
+
+namespace MauiApp1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+
+	}
+}

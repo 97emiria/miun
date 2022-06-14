6c2455d53bda8bbb97c56635f139fc1cf769a94c
Emilia Holmström <e-holmstrom@hotmail.com>
2022-06-14 20:07:54 +0200

    Saving

---
+++ playhouse/vue/crash_vue/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <!-- Header is a component -->
+    <Header title="Task Tracker" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import Header from "./components/Header.vue";
+
+export default defineComponent({
+  name: "App",
+
+  //Import components
+  components: {
+    Header,
+  },
+});
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+  padding: 2em;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #333;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ playhouse/vue/crash_vue/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <ButtonCom text='Add task' color="green" />
+  </header>
+</template>
+
+<script>
+
+import ButtonCom from './ButtonCom.vue'
+
+export default {
+  name: "Header",
+
+  props: {
+    title: {
+      type: String,
+      default: "Missing header",
+    },
+  },
+
+  //Import components
+  components: {
+    ButtonCom
+  }
+
+};
+</script>
+
+<!-- Scoped means only within this component -->
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
